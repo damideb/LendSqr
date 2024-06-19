e384499aaca8e31b8c3b4f6db4849aac07420ab8
Damilola Adebowale <adebzdamzy@gmail.com>
2024-06-19 15:47:46 +0100

    responsnivess and packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './App.scss'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login/Login.tsx'
-
+import Login from './pages/Login/Login'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Error404 from './components/404/Error404';
 
 function App() {
 
@@ -10,6 +11,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='*' element={<Error404 />} />
         </Routes>
       </Router>
   )
